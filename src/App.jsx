99bd8e916d252f0ce2608659171f40beecffb8f9
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-16 18:51:34 +0200

    test git

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <Button variant="outlined">Outlined</Button>
       <AccessAlarmIcon />
       <ThreeDRotation />
     </>
