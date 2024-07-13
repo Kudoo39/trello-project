1d280d8385bae14ecb31c794a81d937d05065098
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2024-07-13 20:17:51 +0300

    Enhance UI/UX drag & drop cards to the empty column

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -5,12 +5,21 @@ import BoardBar from './BoardBar/BoardBar' //flexible relative import
 import BoardContent from './BoardContent/BoardContent' //flexible relative import
 // import { mockData } from '~/apis/mock_data'
 import { fetchBoardDetailsAPI, createNewColumnAPI, createNewCardAPI } from '~/apis'
+import { generatePlaceholderCard } from '~/utils/formatters'
+import { isEmpty } from 'lodash'
 
 const Board = () => {
   const [board, setBoard] = useState(null)
   useEffect(() => {
     const boardId = '6655e094b16640acefaafcba'
     fetchBoardDetailsAPI(boardId).then((board) => {
+      // Handle drag & drop when column is empty, when refresh the page
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        }
+      })
       setBoard(board)
     })
   }, [])
@@ -21,6 +30,16 @@ const Board = () => {
       ...newColumnData,
       boardId: board._id
     })
+
+    // Handle drag & drop when column is empty, when creating a new column
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+
+    // Update state board
+    const newBoard = { ...board }
+    newBoard.columns.push(createdColumn)
+    newBoard.columnOrderIds.push(createdColumn._id)
+    setBoard(newBoard)
   }
 
   // Function: call API to create a new card & refresh State Board
@@ -29,6 +48,15 @@ const Board = () => {
       ...newCardData,
       boardId: board._id
     })
+
+    // Update state board
+    const newBoard = { ...board }
+    const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard)
+      columnToUpdate.cardOrderIds.push(createNewCard._id)
+    }
+    setBoard(newBoard)
   }
 
   return (
