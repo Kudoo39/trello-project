889c7c30dda3c8dc46114ce574e980e0a3ff0c1b
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2025-02-07 01:22:57 +0200

    Sabotage AWS EC2 deployment, use mock data for showcase

--- src/apis/mock_data.js
+++ src/apis/mock_data.js
@@ -6,113 +6,47 @@ export const mockData = {
     type: 'public', // 'private'
     ownerIds: [], // admin users
     memberIds: [], // member users
-    columnOrderIds: ['column-id-01', 'column-id-02', 'column-id-03', 'column-id-04'],
+    columnOrderIds: [
+      'column-id-01',
+      'column-id-02',
+      'column-id-03',
+      'column-id-04'
+    ],
     columns: [
       {
         _id: 'column-id-01',
         boardId: 'board-id-01',
-        title: 'To Do Column 01',
-        cardOrderIds: [
-          'card-id-01',
-          'card-id-02',
-          'card-id-03',
-          'card-id-04',
-          'card-id-05',
-          'card-id-06',
-          'card-id-07'
-        ],
+        title: '💡 Brainstorming',
+        cardOrderIds: ['card-id-01'],
         cards: [
           {
             _id: 'card-id-01',
             boardId: 'board-id-01',
             columnId: 'column-id-01',
-            title: 'Title of card 01',
+            title: 'Khang Nguyen',
             description: 'Markdown Syntax (sẽ ở khóa nâng cao nhé)',
-            cover:
-              'https://trungquandev.com/wp-content/uploads/2022/07/fair-mern-stack-advanced-banner-trungquandev.jpg',
+            cover: 'https://i.imgur.com/VFxNchD.jpeg',
             memberIds: ['test-user-id-01'],
             comments: ['test comment 01', 'test comment 02'],
-            attachments: ['test attachment 01', 'test attachment 02', 'test attachment 03']
-          },
-          {
-            _id: 'card-id-02',
-            boardId: 'board-id-01',
-            columnId: 'column-id-01',
-            title: 'Title of card 02',
-            description: null,
-            cover: null,
-            memberIds: [],
-            comments: [],
-            attachments: []
-          },
-          {
-            _id: 'card-id-03',
-            boardId: 'board-id-01',
-            columnId: 'column-id-01',
-            title: 'Title of card 03',
-            description: null,
-            cover: null,
-            memberIds: [],
-            comments: [],
-            attachments: []
-          },
-          {
-            _id: 'card-id-04',
-            boardId: 'board-id-01',
-            columnId: 'column-id-01',
-            title: 'Title of card 04',
-            description: null,
-            cover: null,
-            memberIds: [],
-            comments: [],
-            attachments: []
-          },
-          {
-            _id: 'card-id-05',
-            boardId: 'board-id-01',
-            columnId: 'column-id-01',
-            title: 'Title of card 05',
-            description: null,
-            cover: null,
-            memberIds: [],
-            comments: [],
-            attachments: []
-          },
-          {
-            _id: 'card-id-06',
-            boardId: 'board-id-01',
-            columnId: 'column-id-01',
-            title: 'Title of card 06',
-            description: null,
-            cover: null,
-            memberIds: [],
-            comments: [],
-            attachments: []
-          },
-          {
-            _id: 'card-id-07',
-            boardId: 'board-id-01',
-            columnId: 'column-id-01',
-            title: 'Title of card 07',
-            description: null,
-            cover: null,
-            memberIds: [],
-            comments: [],
-            attachments: []
+            attachments: [
+              'test attachment 01',
+              'test attachment 02',
+              'test attachment 03'
+            ]
           }
         ]
       },
       {
         _id: 'column-id-02',
         boardId: 'board-id-01',
-        title: 'Inprogress Column 02',
+        title: '🚀 To do',
         cardOrderIds: ['card-id-08', 'card-id-09', 'card-id-10'],
         cards: [
           {
             _id: 'card-id-08',
             boardId: 'board-id-01',
             columnId: 'column-id-02',
-            title: 'Title of card 08',
+            title: 'Learn skills',
             description: null,
             cover: null,
             memberIds: [],
@@ -123,7 +57,7 @@ export const mockData = {
             _id: 'card-id-09',
             boardId: 'board-id-01',
             columnId: 'column-id-02',
-            title: 'Title of card 09',
+            title: 'Database',
             description: null,
             cover: null,
             memberIds: [],
@@ -134,7 +68,7 @@ export const mockData = {
             _id: 'card-id-10',
             boardId: 'board-id-01',
             columnId: 'column-id-02',
-            title: 'Title of card 10',
+            title: 'Clean code',
             description: null,
             cover: null,
             memberIds: [],
@@ -146,14 +80,14 @@ export const mockData = {
       {
         _id: 'column-id-03',
         boardId: 'board-id-01',
-        title: 'Done Column 03',
+        title: '🔧 In progress',
         cardOrderIds: ['card-id-11', 'card-id-12', 'card-id-13'],
         cards: [
           {
             _id: 'card-id-11',
             boardId: 'board-id-01',
             columnId: 'column-id-03',
-            title: 'Title of card 11',
+            title: 'Front-end',
             description: null,
             cover: null,
             memberIds: [],
@@ -164,7 +98,7 @@ export const mockData = {
             _id: 'card-id-12',
             boardId: 'board-id-01',
             columnId: 'column-id-03',
-            title: 'Title of card 12',
+            title: 'Back-end',
             description: null,
             cover: null,
             memberIds: [],
@@ -175,7 +109,7 @@ export const mockData = {
             _id: 'card-id-13',
             boardId: 'board-id-01',
             columnId: 'column-id-03',
-            title: 'Title of card 13',
+            title: 'Testing',
             description: null,
             cover: null,
             memberIds: [],
@@ -187,20 +121,25 @@ export const mockData = {
       {
         _id: 'column-id-04',
         boardId: 'board-id-01',
-        title: 'Empty Column 04',
+        title: '🏆 Complete',
         /*create an special card which is Placeholder Card, not related to Back-end.
         This card will be hidden in UI -> use to fix card cannot drag&drop in empty columns
         -> fix dnd-kit library*/
         cardOrderIds: ['column-id-04-placeholder-card'],
         cards: [
           {
-            _id: 'column-id-04-placeholder-card',
+            _id: 'card-id-14',
             boardId: 'board-id-01',
             columnId: 'column-id-04',
-            FE_PlaceholderCard: true
+            title: 'Project',
+            description: null,
+            cover: null,
+            memberIds: [],
+            comments: [],
+            attachments: []
           }
         ]
       }
     ]
   }
-}
+};
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,165 +1,190 @@
-import { useEffect, useState } from 'react'
-import Container from '@mui/material/Container'
-import AppBar from '~/components/AppBar/AppBar'
-import BoardBar from './BoardBar/BoardBar' //flexible relative import
-import BoardContent from './BoardContent/BoardContent' //flexible relative import
-// import { mockData } from '~/apis/mock_data'
-import { 
+import { useEffect, useState } from 'react';
+import Container from '@mui/material/Container';
+import AppBar from '~/components/AppBar/AppBar';
+import BoardBar from './BoardBar/BoardBar'; //flexible relative import
+import BoardContent from './BoardContent/BoardContent'; //flexible relative import
+import { mockData } from '~/apis/mock_data';
+import {
   fetchBoardDetailsAPI,
   createNewColumnAPI,
   createNewCardAPI,
   updateBoardDetailsAPI,
   updateColumnDetailsAPI,
   moveCardToDifferentColumnAPI,
-  deleteColumnDetailsAPI
-} from '~/apis'
-import { generatePlaceholderCard } from '~/utils/formatters'
-import { isEmpty } from 'lodash'
-import { mapOrder } from '~/utils/sorts'
-import { Box } from '@mui/material'
-import CircularProgress from '@mui/material/CircularProgress'
-import Typography from '@mui/material/Typography'
-import { toast } from 'react-toastify'
+  deleteColumnDetailsAPI,
+} from '~/apis';
+import { generatePlaceholderCard } from '~/utils/formatters';
+import { isEmpty } from 'lodash';
+import { mapOrder } from '~/utils/sorts';
+import { Box } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import { toast } from 'react-toastify';
 
 const Board = () => {
-  const [board, setBoard] = useState(null)
+  const [board, setBoard] = useState(null);
   useEffect(() => {
-    const boardId = '6655e094b16640acefaafcba'
+    const boardId = '6655e094b16640acefaafcba';
     fetchBoardDetailsAPI(boardId).then((board) => {
       // arrange column order before passing to other components
-      board.columns = mapOrder(board.columns, board.columnOrderIds, '_id')
+      board.columns = mapOrder(board.columns, board.columnOrderIds, '_id');
 
-      board.columns.forEach(column => {
+      board.columns.forEach((column) => {
         // Handle drag & drop when column is empty, when refresh the page
         if (isEmpty(column.cards)) {
-          column.cards = [generatePlaceholderCard(column)]
-          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+          column.cards = [generatePlaceholderCard(column)];
+          column.cardOrderIds = [generatePlaceholderCard(column)._id];
         } else {
           // arrange column order before passing to other components
-          column.cards = mapOrder(column.cards, column.cardOrderIds, '_id')
+          column.cards = mapOrder(column.cards, column.cardOrderIds, '_id');
         }
-      })
-      setBoard(board)
-    })
-  }, [])
+      });
+      setBoard(board);
+    });
+  }, []);
 
   // Function: call API to create a new column & refresh State Board
   const createNewColumn = async (newColumnData) => {
     const createdColumn = await createNewColumnAPI({
       ...newColumnData,
-      boardId: board._id
-    })
+      boardId: board._id,
+    });
 
     // Handle drag & drop when column is empty, when creating a new column
-    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
-    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)];
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id];
 
     // Update state board
-    const newBoard = { ...board }
-    newBoard.columns.push(createdColumn)
-    newBoard.columnOrderIds.push(createdColumn._id)
-    setBoard(newBoard)
-  }
+    const newBoard = { ...board };
+    newBoard.columns.push(createdColumn);
+    newBoard.columnOrderIds.push(createdColumn._id);
+    setBoard(newBoard);
+  };
 
   // Function: call API to create a new card & refresh State Board
   const createNewCard = async (newCardData) => {
     const createdCard = await createNewCardAPI({
       ...newCardData,
-      boardId: board._id
-    })
+      boardId: board._id,
+    });
 
     // Update state board
-    const newBoard = { ...board }
-    const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
+    const newBoard = { ...board };
+    const columnToUpdate = newBoard.columns.find(
+      (column) => column._id === createdCard.columnId
+    );
     if (columnToUpdate) {
       // empty column when creating a new card will remove the placeholder card, just add the new created card
-      if (columnToUpdate.cards.some(card => card.FE_PlaceholderCard)) {
-        columnToUpdate.cards = [createdCard]
-        columnToUpdate.cardOrderIds = [createNewCard._id]
+      if (columnToUpdate.cards.some((card) => card.FE_PlaceholderCard)) {
+        columnToUpdate.cards = [createdCard];
+        columnToUpdate.cardOrderIds = [createNewCard._id];
       } else {
-        columnToUpdate.cards.push(createdCard)
-        columnToUpdate.cardOrderIds.push(createNewCard._id)
+        columnToUpdate.cards.push(createdCard);
+        columnToUpdate.cardOrderIds.push(createNewCard._id);
       }
     }
-    setBoard(newBoard)
-  }
+    setBoard(newBoard);
+  };
 
   // Function: call API to handle drag columns in board
   const moveColumns = (dndOrderedColumns) => {
     // update data state board
-    const dndOrderedColumnsIds = dndOrderedColumns.map((c) => c._id)
-    const newBoard = { ...board }
-    newBoard.columns = dndOrderedColumns
-    newBoard.columnOrderIds = dndOrderedColumnsIds
-    setBoard(newBoard)
+    const dndOrderedColumnsIds = dndOrderedColumns.map((c) => c._id);
+    const newBoard = { ...board };
+    newBoard.columns = dndOrderedColumns;
+    newBoard.columnOrderIds = dndOrderedColumnsIds;
+    setBoard(newBoard);
 
     // Call API to update board
-    updateBoardDetailsAPI(newBoard._id, { columnOrderIds: newBoard.columnOrderIds })
-  }
+    updateBoardDetailsAPI(newBoard._id, {
+      columnOrderIds: newBoard.columnOrderIds,
+    });
+  };
 
   // Function: call API to handle drag cards in column
   const moveCardSameColumn = (dndOrderedCards, dndOrderedCardIds, columnId) => {
-    const newBoard = { ...board }
-    const columnToUpdate = newBoard.columns.find(column => column._id === columnId)
+    const newBoard = { ...board };
+    const columnToUpdate = newBoard.columns.find(
+      (column) => column._id === columnId
+    );
     if (columnToUpdate) {
-      columnToUpdate.cards = dndOrderedCards
-      columnToUpdate.cardOrderIds = dndOrderedCardIds
+      columnToUpdate.cards = dndOrderedCards;
+      columnToUpdate.cardOrderIds = dndOrderedCardIds;
     }
-    setBoard(newBoard)
+    setBoard(newBoard);
 
-    updateColumnDetailsAPI(columnId, { cardOrderIds: dndOrderedCardIds })
-  }
+    updateColumnDetailsAPI(columnId, { cardOrderIds: dndOrderedCardIds });
+  };
 
-  const moveCardDifferentColumn = (currentCardId, prevColumnId, nextColumnId, dndOrderedColumns) => {
+  const moveCardDifferentColumn = (
+    currentCardId,
+    prevColumnId,
+    nextColumnId,
+    dndOrderedColumns
+  ) => {
     // update data state board
-    const dndOrderedColumnsIds = dndOrderedColumns.map((c) => c._id)
-    const newBoard = { ...board }
-    newBoard.columns = dndOrderedColumns
-    newBoard.columnOrderIds = dndOrderedColumnsIds
-    setBoard(newBoard)
+    const dndOrderedColumnsIds = dndOrderedColumns.map((c) => c._id);
+    const newBoard = { ...board };
+    newBoard.columns = dndOrderedColumns;
+    newBoard.columnOrderIds = dndOrderedColumnsIds;
+    setBoard(newBoard);
 
     // call API handing from Backend
-    let prevCardOrderIds = dndOrderedColumns.find(c => c._id === prevColumnId)?.cardOrderIds || []
-    
+    let prevCardOrderIds =
+      dndOrderedColumns.find((c) => c._id === prevColumnId)?.cardOrderIds || [];
+
     // Handle issue when dragging the last card out of the column
-    if (prevCardOrderIds[0].includes('placeholder-card')) prevCardOrderIds = []
+    if (prevCardOrderIds[0].includes('placeholder-card')) prevCardOrderIds = [];
 
     moveCardToDifferentColumnAPI({
       currentCardId,
       prevColumnId,
       prevCardOrderIds,
       nextColumnId,
-      nextCardOrderIds: dndOrderedColumns.find(c => c._id === nextColumnId)?.cardOrderIds
-    })
-  }
+      nextCardOrderIds: dndOrderedColumns.find((c) => c._id === nextColumnId)
+        ?.cardOrderIds,
+    });
+  };
 
   const deleteColumnDetails = (columnId) => {
-    const newBoard = { ...board }
-    newBoard.columns = newBoard.columns.filter(column => column._id !== columnId)
-    newBoard.columnOrderIds = newBoard.columnOrderIds.filter(_id => _id !== columnId)
-    setBoard(newBoard)
+    const newBoard = { ...board };
+    newBoard.columns = newBoard.columns.filter(
+      (column) => column._id !== columnId
+    );
+    newBoard.columnOrderIds = newBoard.columnOrderIds.filter(
+      (_id) => _id !== columnId
+    );
+    setBoard(newBoard);
 
-    deleteColumnDetailsAPI(columnId).then(res => {
-      toast.success(res?.deleteResult)
-    })
-
-  }
+    deleteColumnDetailsAPI(columnId).then((res) => {
+      toast.success(res?.deleteResult);
+    });
+  };
 
   if (!board) {
     return (
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 2, width: '100vw', height: '100vh' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 2,
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
         <CircularProgress />
         <Typography>Loading Board...</Typography>
       </Box>
-    )
+    );
   }
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={board} />
+      <BoardBar board={mockData.board} />
       <BoardContent
-        board={board}
+        board={mockData.board}
         createNewColumn={createNewColumn}
         createNewCard={createNewCard}
         moveColumns={moveColumns}
@@ -168,7 +193,7 @@ const Board = () => {
         deleteColumnDetails={deleteColumnDetails}
       />
     </Container>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
